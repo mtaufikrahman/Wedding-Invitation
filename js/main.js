2cf64f72da8a1bbc6b604bc3f501ab1aec1e89d8
mtaufikrahman <mtaufikrahman@ymail.com>
2022-09-02 15:21:56 +0700

    Added functionality to gallery pop up

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(window).on("load", function(){
-    // home section slideshow
+    /************************* home section slideshow *************************/
     let slideIndex = $(".slide.active").index();
     const slideLen = $(".slide").length;
 
@@ -17,9 +17,9 @@ $(window).on("load", function(){
     slideShow();
 })
 
-$(document).ready(function(){
 
-    // family filter
+$(document).ready(function(){
+    /************************* family filter *************************/
     familyFilter($(".filter-btn.active").attr("data-target"))
     $(".filter-btn").click(function(){
         if(!$(this).hasClass("active")){
@@ -32,11 +32,59 @@ $(document).ready(function(){
         $(".family-item").hide();
         $(".family-item[data-category='"+target+"']").fadeIn();
     }
+    
+    /************************* gallery pop-up *************************/
+    const wHeight = $(window).height();
+    $(".gallery-popup .gp-img").css("max-height", wHeight + "px")
 
-    // gallery pop up
-    // const wHeight = $(window).height();
-    // $(".gallery-popup img").css("max-height", wHeight + "px")
+    let gpIndex = 0;
+    const totalGalleryItems = $(".gallery-item").length;
 
-    // let gpIndex = 0;
-    // const totalGalleryItems = $(".gallery-item").length;
+    $(".gallery-item").click(function(){
+        itemIndex = $(this).index();
+        $(".gallery-popup").addClass("open");
+        $(".gallery-popup .gp-img").hide();
+        gpSlideShow();
+    })
+
+    $(".gp-controls .next").click(function(){
+        if(itemIndex == totalGalleryItems - 1){
+            itemIndex = 0;
+        }
+        else{
+            itemIndex++;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function(){
+            gpSlideShow();
+        })
+    })
+
+    $(".gp-controls .prev").click(function(){
+        if(itemIndex == 0){
+            itemIndex = totalGalleryItems - 1;
+        }
+        else{
+            itemIndex--;
+        }
+        $(".gallery-popup .gp-img").fadeOut(function(){
+            gpSlideShow();
+        })
+    })
+
+    function gpSlideShow(){
+        const imgSrc = $(".gallery-item").eq(itemIndex).find("img").attr("data-large");
+        $(".gallery-popup .gp-img").fadeIn().attr("src", imgSrc);
+        $(".gp-counter").text((itemIndex + 1) + "/" + totalGalleryItems);
+    }
+
+    /************************* close gallery pop-up *************************/
+    $(".gp-close").click(function(){
+        $(".gallery-popup").removeClass("open");
+    })
+
+    $(".gallery-popup").click(function(event){
+        if($(event.target).hasClass("open")){
+            $(".gallery-popup").removeClass("open");
+        }
+    })
 })
